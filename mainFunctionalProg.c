b73c9e8bcc6de8fdb800f1df3df85844beb548f1
ervin.domazet@gmail.com <ervin.domazet@gmail.com@d0242072-6afe-f08c-f61d-1be20bd47337>
2011-05-29 18:41:51 +0000

    Main function added

--- mainFunctionalProg.c
+++ mainFunctionalProg.c
@@ -37,3 +37,20 @@ float mult(float num_1,float num_2)
 {
   return num_1*num_2;
 }
+
+
+
+int main()
+{
+  float addition_,subtraction_,multiplication_,division_;
+  addition_=addition( 13.2, 321);
+  subtraction_=subtraction(300.83 ,100.2);
+  multiplication_=mult(19, 0);
+  division_=division(182,31);
+  cout<<"CMPE 352 GROUP 5"<<endl;
+  cout<<"result of addition: "<<addition_<<endl;
+  cout<<"result of subtraction: "<<subtraction_<<endl; 
+  cout<<"result of division: "<<division_<<endl; 
+  cout<<"result of multiplication: "<<multiplication_<<endl; 
+  return 0;
+}
