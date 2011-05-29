a4d7652b9a65cc76d1441dc950fb08c8d8b999ac
bahhti@gmail.com <bahhti@gmail.com@d0242072-6afe-f08c-f61d-1be20bd47337>
2011-05-29 19:16:20 +0000

    New power taking functionality added

--- mainFunctionalProg.c
+++ mainFunctionalProg.c
@@ -46,19 +46,37 @@ float mult(float num_1,float num_2)
   return return_value;
 }
 
+float power(float num_1, int num_2)
+{
+    float temp;
+    if( num_2 == 0)
+       return 1;
+    temp = power(num_1, num_2/2);
+    if (num_2%2 == 0)
+        return temp*temp;
+    else
+    {
+        if(num_2 > 0)
+            return num_1*temp*temp;
+        else
+            return (temp*temp)/num_1;
+    }
+}
 
 
 int main()
 {
-  float addition_,subtraction_,multiplication_,division_;
+  float addition_,subtraction_,multiplication_,division_,power_;
   addition_=addition( 13.2, 321);
   subtraction_=subtraction(300.83 ,100.2);
   multiplication_=mult(19, 0);
   division_=division(182,31);
+  power_=power(12,5);
   cout<<"CMPE 352 GROUP 5"<<endl;
   cout<<"result of addition: "<<addition_<<endl;
   cout<<"result of subtraction: "<<subtraction_<<endl; 
   cout<<"result of division: "<<division_<<endl; 
   cout<<"result of multiplication: "<<multiplication_<<endl; 
+  cout<<"resutl of power:"<<power_<<endl;
   return 0;
 }
