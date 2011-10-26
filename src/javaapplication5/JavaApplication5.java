33937cf32b751edb1244ef2757d0804f51d65811
nurettin.yilmaz <nurettin.yilmaz@boun.edu.tr>
2011-10-26 19:22:07 +0000

    First look from NetBeans

---
+++ src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author px5x2
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
