f6729900296b467c6a56ff8618f5b0256991ad1e
ervin.domazet <ervin.domazet@gmail.com>
2011-11-21 19:36:18 +0000

    ReadAndStorePointsKMLFile(String KML_File) ---- Fonksiyonunu ekledim, KML dosyasindan inputu parse ediyor.

--- Schematizing_Maps/src/schematizing_maps/Schematizing_Maps.java
+++ Schematizing_Maps/src/schematizing_maps/Schematizing_Maps.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package schematizing_maps;
+
 
 /**
  *
@@ -14,9 +14,9 @@ public class Schematizing_Maps {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Applet_1 deneme=new Applet_1();
+    /*public static void main(String[] args) {
+        //Applet_1 deneme=new Applet_1();
         //java.​awt.​event.​ComponentListener
         System.out.println("Hello World!");
-    }
+    }*/
 }
