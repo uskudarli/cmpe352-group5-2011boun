4d4ec81bc80eebad7cdd38b76c53592f0f0febc3
ervin.domazet@gmail.com <ervin.domazet@gmail.com@d0242072-6afe-f08c-f61d-1be20bd47337>
2011-11-29 23:18:18 +0000

    uzun ugrasidan sonra, son hali!

---
+++ trunk/Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/LeftPanel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package applet_algorithm;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author bibi
+ */
+public class LeftPanel extends JPanel{
+    public LeftPanel(){
+    ConfigurationPanel configPanel = new ConfigurationPanel();
+    FileuploadPanel fileUpload = new FileuploadPanel();
+    setLayout(new BorderLayout());
+    add(configPanel,BorderLayout.CENTER);
+    add(fileUpload,BorderLayout.SOUTH);
+    }
+
+}
