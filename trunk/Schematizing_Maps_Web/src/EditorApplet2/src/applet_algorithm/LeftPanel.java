337e2943831fab6fa83feb76abbc1312f6af755c
bahhti@gmail.com <bahhti@gmail.com@d0242072-6afe-f08c-f61d-1be20bd47337>
2011-12-07 15:48:51 +0000

    Configuration panel doesnt show if the user is simple, shows if the user is advanced.

--- trunk/Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/LeftPanel.java
+++ trunk/Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/LeftPanel.java
@@ -17,8 +17,12 @@ public class LeftPanel extends JPanel{
     ConfigurationPanel configPanel = new ConfigurationPanel();
     FileuploadPanel fileUpload = new FileuploadPanel();
     setLayout(new BorderLayout());
-    add(configPanel,BorderLayout.CENTER);
-    add(fileUpload,BorderLayout.SOUTH);
+    if(CanvasApplet.isAdvanced){
+        add(configPanel,BorderLayout.CENTER);
+        add(fileUpload,BorderLayout.SOUTH);
+    } else {
+            add(fileUpload,BorderLayout.CENTER);
+    }
     }
 
 }
