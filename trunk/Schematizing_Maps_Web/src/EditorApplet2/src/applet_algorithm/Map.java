76deb4d4a188f396df5c3e6b8616d629b0d80c4f
nurettin.yilmaz@boun.edu.tr <nurettin.yilmaz@boun.edu.tr@d0242072-6afe-f08c-f61d-1be20bd47337>
2011-12-12 18:56:59 +0000

    SaveMap function and related Runnable class is implemented. Map class getters are coded and new fields defined in class definition.

--- trunk/Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/Map.java
+++ trunk/Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/Map.java
@@ -13,21 +13,44 @@ import java.util.Vector;
 public class Map {
     private Vector<MyPoint> points;
     private Vector<Connection> connections;
-    private String map_name;                    // 
+    private String map_name;                    
     private boolean visible;
+    private String map_owner;           // collected from probably session
+                                        // we need this to relate maps to specific users
+    private String[] keywords;
     
+    /*
+     * Constructor of Map
+     * We are supposed to fill in the fields above.
+     * In fact those are needed for database record.
+     */
+    public Map(){
+        
+    }
     
     
     // @ TODO bahtiyar veya nuretiin yapacak
-    public String returnXMLFormat(){
+    public String getXMLFormat(){
         
         return "";
     }
     
-    public String[] returnKeywords(){
-        return null;
-    }
+    /*
+     * Return array, itll be handled just before it is written to db
+     */
     
+    public String[] getKeywords(){
+        return keywords;
+    }
+    public String getMapName(){
+        return map_name;
+    }
+    public boolean getVisible(){
+        return visible;
+    }
+    public String getMapOwner(){
+        return map_owner;
+    }
     
     
 }
