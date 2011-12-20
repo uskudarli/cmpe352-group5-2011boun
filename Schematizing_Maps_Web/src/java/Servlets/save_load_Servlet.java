0efd967b3c68d858b2c67be8c389a0df04963409
nurettin.yilmaz <nurettin.yilmaz@boun.edu.tr>
2011-12-20 20:47:35 +0000

    mysql_UTIL directory changed.

--- Schematizing_Maps_Web/src/java/Servlets/save_load_Servlet.java
+++ Schematizing_Maps_Web/src/java/Servlets/save_load_Servlet.java
@@ -15,7 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import schematizing_maps.server_side.mysql_UTIL;
+import applet_algorithm.mysql_UTIL;
 
 /**
  *
