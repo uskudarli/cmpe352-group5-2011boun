2307e6a9ed063261afe4b08219c0ab9acfb8859d
bahhti <bahhti@gmail.com>
2011-12-28 21:15:45 +0000

    Export option is available now.

--- Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/CanvasAppletMenuBottom.java
+++ Schematizing_Maps_Web/src/EditorApplet2/src/applet_algorithm/CanvasAppletMenuBottom.java
@@ -13,12 +13,32 @@
 
 package applet_algorithm;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.Transparency;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.jibble.simpleftp.SimpleFTP;
 /**
  *
  * @author bibi
@@ -45,6 +65,11 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
                 saveButtonActionPerformed();
             }
         });
+                exportButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exportButtonActionPerformed(evt);
+            }
+        });
     }
 
     /** This method is called from within the constructor to
@@ -60,6 +85,7 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
         searchButton = new javax.swing.JButton();
         searchKeys = new javax.swing.JTextField();
         saveButton = new javax.swing.JButton();
+        exportButton = new javax.swing.JButton();
 
 
         setBackground(new java.awt.Color(255, 255, 255));
@@ -70,6 +96,7 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
         schematize.setName("schematize"); // NOI18N
         searchButton.setText("Search Maps");
         saveButton.setText("Save Map");
+        exportButton.setText("Export");
        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -79,7 +106,9 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
                 .addComponent(schematize)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(saveButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(exportButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
                 .addComponent(searchKeys, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(27, 27, 27)
                 .addComponent(searchButton)
@@ -93,7 +122,8 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
                     .addComponent(schematize, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
                     .addComponent(searchButton)
                     .addComponent(searchKeys, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(saveButton))
+                    .addComponent(saveButton)
+                    .addComponent(exportButton))
                 .addContainerGap())
         );
     }// </editor-fold>
@@ -158,7 +188,77 @@ public class CanvasAppletMenuBottom extends javax.swing.JPanel {
         schematize.setEnabled(true);
     }
 
+      private void exportButtonActionPerformed(java.awt.event.ActionEvent evt) {
+          //JOptionPane.showMessageDialog(null, "png mi jpg mi");
+          String path="";
+          JFileChooser fc = new JFileChooser();
+          //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+              int returnVal = fc.showOpenDialog(this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                path =file.getAbsolutePath();
+
+            } else {
+            System.out.println("File access cancelled by user.");
+            }
+            int i=0;
+
+            synchronized(this){
+                try{
+                    this.wait(1000);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+              
+
+
+
+                      Robot objRobot = null;
+                 try
+                 {
+                    objRobot = new Robot();
+                 } catch(Exception ex)
+                 {
+
+         }
+
+//Create the image
+BufferedImage exportImage =objRobot.createScreenCapture(new Rectangle(CanvasApplet.canvasPanel.getLocationOnScreen().x,CanvasApplet.canvasPanel.getLocationOnScreen().y, CanvasApplet.canvasPanel.getLocationOnScreen().x+CanvasApplet.canvasPanel.getSize().width, CanvasApplet.canvasPanel.getLocationOnScreen().y+CanvasApplet.canvasPanel.getSize().height-80));
+
+
+//Setup to write the BufferedImage to a file
+String pathToFile = path;
+File outputDirectory = new File(pathToFile);
+File outputFile = new File(pathToFile+".png");
+//Here we make sure the directory exists.
+/*
+ * Returns TRUE if:
+ *  The directory is MISSING
+ *  and/or the directory IS NOT a directory
+ */
+if(!outputDirectory.exists() || !outputDirectory.isDirectory()){
+    outputDirectory.mkdirs(); //Make the directory
+} // Else do nothing
+
+//Write the file
+try { //Attempt the write
+    ImageIO.write(exportImage, "png", outputFile);
+} catch (IOException e) { //For some reason it failed so...
+    e.printStackTrace(); //... why did it fail?
+}
+
+
+
+        }
+
+
+
+
+
+
     // Variables declaration - do not modify
+    private javax.swing.JButton exportButton;
     private javax.swing.JButton schematize;
     private javax.swing.JButton saveButton;
     private javax.swing.JButton searchButton;
